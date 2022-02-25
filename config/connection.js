3591ad24cc3d543c394e004a3ddb9d0e062703d2
Grant Royseth <groyseth@gmail.com>
2022-02-24 20:22:22 -0600

    commit

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/pianoPage', {
+connect(process.env.MONGODB_URI||'mongodb://localhost/pianoPage', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
